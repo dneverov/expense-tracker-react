952c6828d661f7e17571390ea22fa9b941c2f1e2
Denis Neverov <denis.neverov@gmail.com>
2020-06-06 18:11:03 +0300

    Add add/delete Transaction functions

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -18,8 +18,25 @@ export const GlobalContext = createContext(initalState);
 export const GlobalProvider = ({children}) => {
   const [state, dispatch] = useReducer(AppReducer, initalState);
 
+  // Actions
+  function deleteTransaction(id){
+    dispatch({
+      type: 'DELETE_TRANSACTION',
+      payload: id
+    });
+  }
+
+  function addTransaction(transaction){
+    dispatch({
+      type: 'ADD_TRANSACTION',
+      payload: transaction
+    });
+  }
+
   return (<GlobalContext.Provider value={{
-    transactions: state.transactions
+    transactions: state.transactions,
+    deleteTransaction,
+    addTransaction
   }}>
     {children}
   </GlobalContext.Provider>);
